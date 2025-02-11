4b45bf820974ee8e74bfd6a7266e5e1a26744dea
TNHo <88540087+TNHo@users.noreply.github.com>
2025-02-11 10:20:55 -0500

    update

---
+++ CPP/TwoSum.cpp
@@ -0,0 +1,95 @@
+/*
+ * Problem: Two Sum
+ *
+ * Problem Description:
+ * You’re given:
+ * An array of integers (nums)
+ * A target integer (target)
+ * Your task:
+ * Write a function that finds two numbers in the array that add up to the target and returns their indices.
+ *
+ * Example:
+ * Input: nums = [2, 7, 11, 15], target = 9
+ * Output: [0, 1]
+ * Why? nums[0] + nums[1] = 2 + 7 = 9
+ *
+ * Important Notes:
+ * There is exactly one solution for each input.
+ * You can’t use the same number twice.
+ * The answer can be in any order.
+ */
+#include<iostream>
+#include <vector>
+using namespace std;
+
+vector<int> twoSum(vector<int>&, int);
+
+// The start of the method you see in the real leetcode starting template
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        // Start of my code
+        int sum = 0, k, i;
+        vector<int> solution;
+        for (k = 0; k < nums.size(); k++) {
+            for (i = 0; i < nums.size(); i++) {
+                if (k!=i) // Make sure the same elements are not used
+                    sum = nums.at(k) + nums.at(i);
+                if (sum==target) {
+                    solution.push_back(k); // Adding to vector
+                    break;
+                }
+            }
+        }
+        return solution;
+    }
+};
+
+// Just something we need so we can see the output
+// Boilerplate for running testcases outside the leetcode site
+int main() {
+    Solution s;
+    std::vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    std::vector<int> result = s.twoSum(nums, target);
+
+    if (!result.empty()) {
+        std::cout << "Indices: ";
+        for (int index : result) {
+            std::cout << index << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "No solution found." << std::endl;
+    }
+
+
+    nums = {3,2,4};
+    target = 6;
+    result = s.twoSum(nums, target);
+
+    if (!result.empty()) {
+        std::cout << "Indices: ";
+        for (int index : result) {
+            std::cout << index << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "No solution found." << std::endl;
+    }
+
+    nums = {3,3};
+    target = 6;
+    result = s.twoSum(nums, target);
+
+    if (!result.empty()) {
+        std::cout << "Indices: ";
+        for (int index : result) {
+            std::cout << index << " ";
+        }
+        std::cout << std::endl;
+    } else {
+        std::cout << "No solution found." << std::endl;
+    }
+    return 0;
+}
